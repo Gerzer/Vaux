fd53e17161def8d09729a85f0d8609ff660ca240
Gerzer <gerzersoftware@icloud.com>
2019-08-01 12:55:47 -0400

    Format Package.swift

--- Package.swift
+++ Package.swift
@@ -1,16 +1,27 @@
 // swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "Vaux",
-    products: [
-        .library(name: "Vaux", targets: ["Vaux"])
-    ],
-    dependencies: [],
-    targets: [
-        .target(name: "Vaux", dependencies: []),
-        .testTarget(name: "VauxTests", dependencies: ["Vaux"])
-    ]
+	name: "Vaux",
+	products: [
+		.library(
+			name: "Vaux",
+			targets: [
+				"Vaux"
+			]
+		)
+	],
+	dependencies: [],
+	targets: [
+		.target(
+			name: "Vaux"
+		),
+		.testTarget(
+			name: "VauxTests",
+			dependencies: [
+				"Vaux"
+			]
+		)
+	]
 )
